f0dfff8c2aa76eeaa4e82a7c63fad7d3af43d02b
lqvinh2 <lqvinh.hsu@gmail.com>
2021-04-22 16:15:16 +0700

    learning
    learning

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="p in posts" :key="p.id">
+     <SinglePost :post="p"></SinglePost>
+  </div>
+</template>
+
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+
+import SinglePost from '../components/SinglePost.vue'
+export default {
+    name : "PostList",
+    components : {SinglePost},
+      props: ['posts'],
+
+   setup(props) {
+    onMounted(() => console.log('component mounted'))
+    onUnmounted(() => console.log('component unmounted'))
+    onUpdated(() => console.log('component updated'))
+  }
+
+}
+</script>
+
+<style>
+
+</style>
