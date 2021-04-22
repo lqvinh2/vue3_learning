a3602e3afeb9d95d0f6dd918759b35719f6dd0fb
lqvinh2 <lqvinh.hsu@gmail.com>
2021-04-22 10:22:00 +0700

    create new project
    tạo server ảo bằng npm install json-server

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>{{titleApp}}</h1>
-
-  <div class="" v-if="isShowModal">
-     <Modal :title="title" :header="header" theme="sale" @App_CloseModal="ToogleModal"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> 
   </div>
-
-  <button type="button" class="btn btn-secondary" @click="ToogleModal">Show modal</button>
- 
+  <router-view/>
 </template>
 
-<script>
-import Modal from "./components/Modal.vue";
 
 
-export default {
-  name: "App",
 
-  data(){
-    return{
-    titleApp : "app title yeye",
-    title : "app title yeye",
-    header : "modal header",
-    isShowModal:false
-
-  }},
-
-  methods:{
-    ToogleModal()
-    {
-      this.isShowModal = !this.isShowModal;
-    }
-  },
-
-
-  components: {
-    Modal
-  },
-};
-</script>
 
 <style>
+
+body {
+  background: #e5e2e2;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  max-width: 600px;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
