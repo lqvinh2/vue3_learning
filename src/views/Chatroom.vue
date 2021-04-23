216b5a4aba9b723b6492ebb453402222937287d0
qmloffice <lqvinh.hsu@gmail.com>
2021-04-23 23:28:46 +0700

    ssss
    sss

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,14 +1,38 @@
 <template>
-  <div class="container">
+  <div v-if="isShow" class="container">
     <Navbar />
+    <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import NewChatForm from "../components/NewChatForm.vue";
+import getUser from "../composables/getUser";
+import { computed, watch } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
+  setup() {
+    const router = useRouter();
+    const { user } = getUser();
+
+    const isShow = computed(() => {
+      if (!user.value) {
+        isShow = false;
+      } else {
+        isShow = true;
+      }
+    });
+
+    // if the user value is ever null, redirect to welcome screen
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+  },
 };
 </script>
 
