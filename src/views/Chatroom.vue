7227ab8cae3182e17427eb2f13152ce7fada5214
qmloffice <lqvinh.hsu@gmail.com>
2021-04-23 23:46:05 +0700

    tạo 1 component chat khi nhấn enter sẽ hiện tên của người gửi và message
    tạo 1 component chat khi nhấn enter sẽ hiện tên của người gửi và message

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="isShow" class="container">
+  <div class="container">
     <Navbar />
     <NewChatForm></NewChatForm>
   </div>
@@ -9,7 +9,7 @@
 import Navbar from "../components/Navbar.vue";
 import NewChatForm from "../components/NewChatForm.vue";
 import getUser from "../composables/getUser";
-import { computed, watch } from "vue";
+import { computed, watch, ref } from "vue";
 import { useRouter } from "vue-router";
 
 export default {
@@ -18,20 +18,19 @@ export default {
     const router = useRouter();
     const { user } = getUser();
 
-    const isShow = computed(() => {
-      if (!user.value) {
-        isShow = false;
-      } else {
-        isShow = true;
-      }
-    });
+    const isShow = ref(false);
 
     // if the user value is ever null, redirect to welcome screen
     watch(user, () => {
       if (!user.value) {
-        router.push({ name: "Welcome" });
+        //router.push({ name: "Welcome" });
+        isShow.value = false;
+      } else {
+        isShow.value = true;
       }
     });
+
+    return { isShow };
   },
 };
 </script>
